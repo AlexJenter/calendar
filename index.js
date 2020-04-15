d313ac011cc9d4785fe531847b679bf1aba00a15
Alex Jenter <jenteralex@gmail.com>
2020-04-15 11:43:50 +0200

    Adds ERZ

--- index.js
+++ index.js
@@ -2,39 +2,22 @@ const { Parser } = require("./src/Parser");
 const { icsExporter } = require("./src/icsExporter");
 
 const generator = (icsPath, filePaths) => {
-
   let parser = new Parser();
-  filePaths.map(path => parser.parse(path))
+  filePaths.map((path) => parser.parse(path));
 
   let ics = new icsExporter(parser.events);
   ics.ics(icsPath);
-
-}
-
+};
 
 // https://signalwerk.github.io/calendar/PublicHoliday.ics
-generator(
-  "./public/PublicHoliday.ics",
-  [
-    "./data/Feiertage/feiertage.txt",
-    "./data/Feiertage/knabenschiessen.txt",
-    "./data/Feiertage/sechseläuten.txt",
-  ]
-)
+generator("./public/PublicHoliday.ics", [
+  "./data/Feiertage/feiertage.txt",
+  "./data/Feiertage/knabenschiessen.txt",
+  "./data/Feiertage/sechseläuten.txt",
+]);
 
 // https://signalwerk.github.io/calendar/PublicHoliday.ics
-generator(
-  "./public/Physio.ics",
-  [
-    "./data/Physio/Therapieplan-618417.txt",
-  ]
-)
+generator("./public/Trash.ics", ["./data/Entsorgung/index.txt"]);
 
 // https://alexjenter.github.io/calendar/projekt-woche-sfgz.ics
-generator(
-  "./public/projekt-woche-sfgz.ics",
-  [
-    "./data/sfgz/projekt-woche.txt",
-  ]
-)
-
+generator("./public/projekt-woche-sfgz.ics", ["./data/sfgz/projekt-woche.txt"]);
